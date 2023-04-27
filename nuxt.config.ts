c0589130565cb25f86ad88ddbf2e211bc7586bcd
buggio.dev <buggio.dev@gmail.com>
2023-04-27 13:33:13 +0200

    add '@nuxtjs/composition-api/module'

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,62 +1,63 @@
-import type { NuxtConfig } from '@nuxt/types'
+import type { NuxtConfig } from '@nuxt/types';
 
 const config: NuxtConfig = {
-    telemetry: false, //https://github.com/nuxt/telemetry
+    telemetry: false, // https://github.com/nuxt/telemetry
 
     // Global page headers: https://go.nuxtjs.dev/config-head
-  head: {
-    title: 'nuxt-bootstrap',
-    htmlAttrs: {
-      lang: 'en'
+    head: {
+        title: 'nuxt-bootstrap',
+        htmlAttrs: {
+            lang: 'en'
+        },
+        meta: [
+            { charset: 'utf-8' },
+            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+            { hid: 'description', name: 'description', content: '' },
+            { name: 'format-detection', content: 'telephone=no' }
+        ],
+        link: [
+            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+        ]
     },
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+
+    // Global CSS: https://go.nuxtjs.dev/config-css
+    css: [
+        '~/assets/scss/index.scss'
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
-  },
 
-  // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-      '~/assets/scss/index.scss'
-  ],
+    // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+    plugins: [
+    ],
 
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+    // Auto import components: https://go.nuxtjs.dev/config-components
+    components: true,
 
-  // Auto import components: https://go.nuxtjs.dev/config-components
-  components: true,
+    // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+    buildModules: [
+        '@nuxt/typescript-build',
+        '@nuxtjs/composition-api/module',
+    ],
 
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-      '@nuxt/typescript-build'
-  ],
+    // Modules: https://go.nuxtjs.dev/config-modules
+    modules: [
+    ],
 
-  // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-  ],
-
-  // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-      loaders: {
-          scss: {
-              additionalData: `
+    // Build Configuration: https://go.nuxtjs.dev/config-build
+    build: {
+        loaders: {
+            scss: {
+                additionalData: `
               @import "~@/assets/scss/variables"; // override bootstrap variables and load to vue components
               @import "~bootstrap/scss/functions";
               @import "~bootstrap/scss/variables";
              `,
-              sassOptions: {
-                  // indentedSyntax: true,
-              },
-          }
-      },
+                sassOptions: {
+                    // indentedSyntax: true,
+                },
+            }
+        },
 
-  }
-}
+    }
+};
 
-export default config
+export default config;
